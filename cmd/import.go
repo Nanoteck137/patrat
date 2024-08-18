45bc822f7eef7f68edbd5a38429bfb97c510cc2b
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-08-19 00:04:33 +0200

    Working on the import process of cbz files

--- cmd/import.go
+++ cmd/import.go
@@ -1,25 +1,188 @@
 package cmd
 
 import (
+	"archive/zip"
+	"encoding/xml"
+	"fmt"
+	"io"
 	"log"
+	"os"
+	"path"
+	"slices"
+	"strings"
 
 	"github.com/kr/pretty"
 	"github.com/nanoteck137/sewaddle-core/library"
+	"github.com/pelletier/go-toml/v2"
 	"github.com/spf13/cobra"
 )
 
 var importCmd = &cobra.Command{
 	Use: "import",
+	// Run: func(cmd *cobra.Command, args []string) {
+	// 	lib, err := library.ReadFromDir("/Volumes/media/manga")
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	//
+	// 	// lib.AddSerie()
+	//
+	// 	pretty.Println(lib)
+	// },
+}
+
+var importCbzCmd = &cobra.Command{
+	Use:  "cbz <CBZ_FILE>",
+	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		lib, err := library.ReadFromDir("/Volumes/media/manga")
+		fmt.Printf("args[0]: %v\n", args[0])
+
+		lib, err := library.ReadFromDir("./work")
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		pretty.Println(lib)
+		_ = lib
+
+		r, err := zip.OpenReader(args[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer r.Close()
+
+		var comicInfo *zip.File
+
+		for _, f := range r.File {
+			if f.Name == "ComicInfo.xml" {
+				comicInfo = f
+			}
+		}
+
+		if comicInfo != nil {
+			var pages []string
+			for _, f := range r.File {
+				ext := path.Ext(f.Name)
+				if ext == ".jpeg" || ext == ".jpg" || ext == ".png" {
+					pages = append(pages, f.Name)
+				}
+			}
+
+			fmt.Println("Found ComicInfo.xml")
+
+			reader, err := comicInfo.Open()
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer reader.Close()
+
+			data, err := io.ReadAll(reader)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			type ComicInfo struct {
+				Title  string `xml:"Title"`
+				Series string `xml:"Series"`
+			}
+
+			var info ComicInfo
+			err = xml.Unmarshal(data, &info)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			pretty.Println(info)
+
+			chapterSlug := library.Slug(info.Title)
+			serieSlug := library.Slug(info.Series)
+
+			fmt.Printf("info.Series: %v\n", info.Series)
+			fmt.Printf("library.Slug(info.Series): %v\n", library.Slug(info.Series))
+			fmt.Printf("library.Slug(info.Title): %v\n", library.Slug(info.Title))
+
+			var serie library.SerieMetadata
+			found := false
+
+			for _, s := range lib.Series {
+				if s.Slug == serieSlug {
+					serie = s
+					found = true
+					break
+				}
+			}
+
+			if found {
+				pretty.Println(serie)
+
+				for _, c := range serie.Chapters {
+					if c.Slug == chapterSlug {
+						log.Fatalf("Chapter already exists: '%s'", chapterSlug)
+					}
+				}
+
+				serie.Chapters = append(serie.Chapters, library.ChapterMetadata{
+					Slug:  chapterSlug,
+					Name:  info.Title,
+					Pages: pages,
+				})
+
+				slices.SortFunc(serie.Chapters, func(a, b library.ChapterMetadata) int {
+					return strings.Compare(a.Slug, b.Slug)
+				})
+
+				p := serie.Path()
+
+				d, err := toml.Marshal(serie)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				fmt.Printf("string(d): %v\n", string(d))
+
+				err = os.WriteFile(path.Join(p, "manga.toml"), d, 0644)
+				if err != nil {
+					log.Fatal(err)
+				}
+			} else {
+				p := path.Join(lib.Base, serieSlug)
+				err = os.Mkdir(p, 0755)
+				if err != nil {
+					if !os.IsExist(err) {
+						log.Fatal(err)
+					}
+				}
+
+				serie := library.SerieMetadata{
+					Slug:     serieSlug,
+					Title:    info.Series,
+					CoverArt: "",
+					Chapters: []library.ChapterMetadata{
+						{
+							Slug:  library.Slug(info.Title),
+							Name:  info.Title,
+							Pages: pages,
+						},
+					},
+				}
+
+				d, err := toml.Marshal(serie)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				fmt.Printf("string(d): %v\n", string(d))
+
+				err = os.WriteFile(path.Join(p, "manga.toml"), d, 0644)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+		}
 	},
 }
 
 func init() {
+	importCmd.AddCommand(importCbzCmd)
+
 	rootCmd.AddCommand(importCmd)
 }
--- go.mod
+++ go.mod
@@ -6,12 +6,14 @@ toolchain go1.21.7
 
 require (
 	github.com/kr/pretty v0.3.1
-	github.com/nanoteck137/sewaddle-core v0.1.0
+	github.com/nanoteck137/sewaddle-core v0.2.1
+	github.com/pelletier/go-toml/v2 v2.2.2
 	github.com/spf13/cobra v1.8.1
 )
 
 require (
-	github.com/flytam/filenamify v1.2.0 // indirect
+	github.com/gosimple/slug v1.14.0 // indirect
+	github.com/gosimple/unidecode v1.0.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/rogpeppe/go-internal v1.9.0 // indirect
